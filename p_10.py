5a6cf8daeba93e8cd936685087b3d7d74e94edca
Midhun Pradeep <midhun128@gmail.com>
2020-04-03 11:59:51 +0300

    Move function to module my_functions

--- p_10.py
+++ p_10.py
@@ -6,27 +6,10 @@ Find the sum of all the primes below two million.
 Answer: 142913828922
 """
 
-from math import sqrt
-
-
-def get_prime_array(n):
-
-    A = [False, False] + [True for _ in range(2, n + 1)]
-
-    i = 2
-    while i <= sqrt(n):
-        if A[i]:
-            j = i**2
-            while j <= n:
-                A[j] = False
-                j += i
-        i += 1
-
-    return [i for i, is_prime in enumerate(A) if is_prime]
-
+import my_functions
 
 n = int(input("N: "))
 if n <= 1:
     raise ValueError("n cannot be less than 2.")
 
-print(sum(get_prime_array(n)))
+print(sum(my_functions.get_prime_list(n)))
