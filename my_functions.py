01805feb7461596df2ea05e4d417bf94024d70f8
Midhun Pradeep <midhun128@gmail.com>
2020-04-03 12:52:55 +0300

    Add function number_of_factors

--- my_functions.py
+++ my_functions.py
@@ -1,7 +1,8 @@
+from collections import Counter
 from math import sqrt
 
 
-def get_prime_array(n):
+def get_prime_list(n):
     """
     Return a list of prime numbers using the Sieve of Eratosthenes
     """
@@ -18,3 +19,37 @@ def get_prime_array(n):
         i += 1
 
     return [i for i, is_prime in enumerate(A) if is_prime]
+
+
+def number_of_factors(n, return_factors=False):
+    """
+    Return the number of factors of a number.
+
+    Uses the formula:
+    Number of factors = (1+a1) * (1+a2) * (1+a3) * … (1+an)
+    where a1, a2, a3 …. an are count of distinct prime factors of the number.
+    """
+
+    from math import sqrt
+
+    prime_factors = Counter()
+
+    while n % 2 == 0:  # Finds the number of times n can be divided by 2.
+        prime_factors[2] += 1
+        n //= 2
+
+    i = 3
+    while n > 1:  # Finds the number of times n can be divided by every prime odd number > 2.
+        while n % i == 0:
+            prime_factors[i] += 1
+            n //= i
+        i += 2
+
+    factor_count = 1
+    for count in prime_factors.values():
+        factor_count *= (count + 1)
+
+    if return_factors:
+        return factor_count, prime_factors
+    else:
+        return factor_count
